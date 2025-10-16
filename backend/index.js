3e52afc6adba75e0db0b1eebbd7bd8c27c964736
pari1011 <9eparitibrewal@gmail.com>
2025-10-16 12:14:23 +0530

    feat: added multer setup and /upload route

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,37 @@
 const express=require('express')
 const app=express()
+//importing multer to upload files
+const multer=require("multer")
+
+
+//temporay memory storage
+//uploaded file will be kept in ram instead of storing to disk since it will be sent to supabase later
+const storage=multer.memoryStorage()
+
+//a middleware that will intercept the request and make the uploaded file available as req.file
+const upload=multer({storage:storage})
 const PORT= 5000
 
-app.get('/test', (req,res)=>{
-    res.json({message: "hello from backend"})
+//defining a post route at upload
+app.post('/upload', upload.single('file'),async(req,res)=>{
+   
+    const file=req.file //req.file contains details about the file name,size,etc.
+    console.log(file)
+    if(!file){
+        return res.status(400).json({error: "No file uploaded"}) //incase no file is uploaded
+
+    }
+    res.status(200).json({
+        message: "file uploaded successfully",
+    })
+    
+    
 })
+
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`server started at port: ${PORT}`)

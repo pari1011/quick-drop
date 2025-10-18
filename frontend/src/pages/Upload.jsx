dfcf1e31176b4f53cba87919e06bb033a76c30d6
pari1011 <9eparitibrewal@gmail.com>
2025-10-18 20:53:00 +0530

    feat: integrate upload API with backend and display generated link

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -16,31 +16,55 @@ const Upload = () => {
   }
   //event handler for expiry time
   const [expiryTime, setexpiryTime] = useState(0);
-  const handleExpiryTime=(e)=>{
-             const time=e.target.value.split("min")[0]
-             setexpiryTime(Number(time))
-           
+  const handleExpiryTime = (e) => {
+    const val = e.target.value
+    if (val.endsWith("min")) {
+    const time = val.split("min")[0];
+    setexpiryTime(Number(time));
+    }else if (val.endsWith("hr")) {
+    const time = val.split("hr")[0];
+    setexpiryTime(Number(time) * 60);
   }
 
+             
+           
+  }
+  const [message, setmessage] = useState("");
+  const [url, seturl] = useState(null);
   const handleUpload=()=>{
+
     const formData= new FormData()
     formData.append("file", file)
     formData.append("expiry_time", expiryTime)
     axios.post('http://localhost:5000/upload', formData)
     .then(res => {
     console.log('Upload success:', res.data);
+    setmessage(res.data.message)
+    seturl(res.data.link)
      })
     .catch(err => {
     console.error('Upload failed:', err);
     });
+
     
     
+  }
+  
+  const copyToClipboard=()=>{
+    navigator.clipboard.writeText(url)
+    .then(() => {
+      alert("Link copied to clipboard!");
+    })
+    .catch((err) => {
+      console.error("Failed to copy: ", err);
+    });
+
   }
 
 
   return (
     <div className='bg-[#0B1A33] w-full h-screen flex items-center justify-center'>
-      <div className=' bg-gray-700 rounded-3xl text-center p-10 flex flex-col w-100 items-center '>
+      <div className=' bg-gray-700 rounded-3xl text-center p-10 flex flex-col w-150 items-center '>
         <h1 className='text-4xl bold text-white'> Quick Drop</h1>
         <label
          for="file-upload"
@@ -60,16 +84,28 @@ const Upload = () => {
           <option className=' text-white rounded-l bg-gray-500 '>10min</option>
           <option className=' text-white rounded-l bg-gray-500'>15min</option>
           <option className='text-white rounded-l bg-gray-500'>30min</option>
-          <option className=' text-white rounded-l bg-gray-500'>1hour</option>
-          <option className=' text-white rounded-l bg-gray-500'>2hour</option>
+          <option className=' text-white rounded-l bg-gray-500'>1hr</option>
+          <option className=' text-white rounded-l bg-gray-500'>2hr</option>
           <option className=' text-white rounded-l bg-gray-500'>6hr</option>
           <option className='text-white rounded-l bg-gray-500'>24hr</option>
 
         </select>
         
         <button className='mt-8 bg-blue-600 text-white rounded-md p-1 w-full cursor-pointer' onClick={handleUpload}>Upload File</button>
-
+        {url?(
+           <div className='mt-8'> 
+          <p className='text-gray-300'>{message}</p>
+          <p className='text-gray-300'>Here's your generated URl: </p>
+          <div className='flex items-center justify-between gap-8 mt-3'>
+             <a href={url} className='text-white rounded-l bg-gray-500 p-1'>{url}</a>
+            <button className='bg-blue-600 text-white rounded-md  cursor-pointer p-1' onClick={copyToClipboard}> Copy Link</button>
+          </div>
+       </div>
+        ):(<> </>)}
+         
+        
       </div>
+      
     </div>
   )
 }
